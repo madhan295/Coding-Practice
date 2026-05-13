2b01193b3c39ff5234884fcf4f703a8a3dea3552
Madhankannan02 <madhankannan29@gmail.com>
2026-05-13 20:14:04 +0530

    3f6f4c2

---
+++ FreeCodeCamp/Front-End Development Libraries/React Fundamentals/02.Build-a-Resuable-Footer/index.jsx
@@ -0,0 +1,26 @@
+export  function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>Products</li>
+        <li>Solutions</li>
+      </ul>
+
+      <ul>
+        <li>Documentation</li>
+        <li>API Reference</li>
+      </ul>
+
+      <ul>
+        <li>Privacy Policy</li>
+        <li>Terms of Use</li>
+      </ul>
+
+      <p>Copyright © 2026 TechFlow Inc.</p>
+
+      <a href="#">Facebook</a>
+      <a href="#">Instagram</a>
+      <a href="#">Discord</a>
+    </footer>
+  );
+}
